f28f0778e93261b6071550531963b342ebe62ee4
Franklin Yin <yin4thewin@gmail.com>
2021-06-12 13:05:52 -0400

    First commit

---
+++ index.js
@@ -0,0 +1,59 @@
+var admin = require('firebase-admin');
+var serviceAccount = require("./debtbalance-fe09f-firebase-adminsdk-40051-6bb4c2b643.json");
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const port = 3000
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+const db=admin.firestore()
+var jsonParser = bodyParser.json()
+ 
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
+
+app.get('/', jsonParser, (req,res)=>{
+    let users={}
+    let promises=[]
+    let name=""
+    let numbers=req.body.numbers
+    numbers.sort((a,b)=>{
+        return a-b
+    })
+    numbers.forEach(number=>{
+        promises.push(db.collection('Users').doc(number).get().then(doc=>{
+            if(doc.exists){
+                name+=number
+                users[number]={name: doc.data().name, balance: 0}
+                console.log(users)
+            }
+        }))
+    })
+    Promise.all(promises).then(()=>{
+        db.collection('Groups').doc(name).get().then(doc=>{
+            if(doc.exists){
+                console.log("Exists")
+            }else{
+                db.collection('Groups').doc(name).set({
+                    members: users,
+                    transactions: []
+                }).then(()=>{
+                    req.body.numbers.forEach(number=>{
+                        db.collection('Users').doc(number).get().then(doc=>{
+                            if(doc.exists){
+                                db.collection('Users').doc(number).update({
+                                    groups: admin.firestore.FieldValue.arrayUnion(name)
+                                })
+                            }
+                        })
+                    })
+                })
+            }
+        })
+    })
+    return res.json(numbers)
+})
