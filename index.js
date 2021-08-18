2654caa85775c275809274e2b81edcc884b636fe
Franklin Yin <yin4thewin@gmail.com>
2021-08-18 13:56:48 -0400

    Leaving groups should work

--- index.js
+++ index.js
@@ -34,6 +34,50 @@ app.get('/check', (req, res)=>{
     return res.send(cachedUsers.includes(number))
 })
 
+app.get('/leave', (req,res)=>{
+    let error="Unsuccess"
+    let number=req.query.number, group=req.query.group
+    if(!number||!group)
+        return res.send("Invalid fields")
+    error=number
+    db.collection('Users').doc(number).get().then(userDoc=>{
+        if(userDoc.exists){
+            db.collection('Groups').doc(group).get().then(groupDoc=>{
+                if(groupDoc.exists){
+                    console.log("Group exists")
+                    let members=groupDoc.data().members
+                    if(number in members){
+                        console.log("Number in members",  members[number])
+                        delete members[number]
+                        console.log(members)
+                        db.collection('Users').doc(number).update({groups: admin.firestore.FieldValue.arrayRemove(groupDoc.id)})
+                        let newID=groupDoc.id.replace(number,"")
+                        db.collection('Groups').doc(newID).set({
+                            members: members,
+                            transactions: groupDoc.data().transactions
+                        })
+                        let promises=[]
+                        Object.keys(members).forEach(member=>{
+                            promises.push(db.collection('Users').doc(member).update({groups: admin.firestore.FieldValue.arrayRemove(groupDoc.id)}))
+                            promises.push(db.collection('Users').doc(member).update({groups: admin.firestore.FieldValue.arrayUnion(newID)}))
+                        })
+                        Promise.all(promises).then(()=>{
+                            db.collection('Groups').doc(group).delete().then(()=>{
+                                error="success"
+                            })
+                        })
+                    }
+                }
+                error="Group not found"
+            })    
+        }
+        error="User not found"
+    }).catch((err)=>{
+        error=err
+    })
+    return res.send(error) 
+})
+
 app.post('/', jsonParser, (req,res)=>{
     let users={}
     let promises=[]
