1e99699fac7baaa728e242366e55d8194c21c455
KihtrakRaknas <karthikandsankar@gmail.com>
2021-08-18 14:19:40 -0400

    return error message if error on /

--- index.js
+++ index.js
@@ -66,6 +66,7 @@ app.post('/', jsonParser, (req,res)=>{
         db.collection('Groups').doc(name).get().then(doc=>{
             if(doc.exists){
                 console.log("Exists")
+                res.json({error: "Group already exists"})
             }else{
                 db.collection('Groups').doc(name).set({
                     members: users,
@@ -80,9 +81,10 @@ app.post('/', jsonParser, (req,res)=>{
                             }
                         })
                     })
+                    res.json({status: "successful"})
                 })
             }
         })
     })
-    return res.json(numbers)
+    
 })
